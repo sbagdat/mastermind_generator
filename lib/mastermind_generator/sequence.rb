bf6aab3e9dd5f2c41a9dbeebd1edde653547325e
Sıtkı Bağdat <sbagdat@gmail.com>
2021-03-10 06:25:14 +0300

    feat: Add validation and comparison to sequences

--- lib/mastermind_generator/sequence.rb
+++ lib/mastermind_generator/sequence.rb
@@ -1,17 +1,53 @@
 # frozen_string_literal: true
 
 module MastermindGenerator
+  class SequenceTooShortError < StandardError; end
+
+  class SequenceTooLongError < StandardError; end
+
+  class SequenceHasInvalidCharacterError < StandardError; end
+
   # Respresents valid sequence that created according to difficulty level
   class Sequence
-    attr_reader :items
+    attr_reader :items, :value, :difficulty
 
-    def initialize(difficulty)
-      items = MastermindGenerator.configuration.items[..difficulty.item_count - 1]
-      @items = items.map{ Item.new(_1) }
+    def initialize(difficulty, value)
+      @difficulty = difficulty
+      @items = MastermindGenerator
+               .configuration
+               .items[..difficulty.item_count - 1]
+               .map { Item.new(_1) }
+      @value = validate(value)
     end
 
     def codes
       items.map(&:code)
+    end
+
+    def ==(other)
+      value == other.value
+    end
+
+    private
+
+    def validate(value)
+      raise(SequenceTooLongError) if long?(value)
+      raise(SequenceTooShortError) if short?(value)
+      raise(SequenceHasInvalidCharacterError) if contains_invalid_chars?(value)
+
+      value
+    end
+
+    def contains_invalid_chars?(value)
+      value.chars.any? { !codes.include?(_1) }
+    end
+
+    def short?(value)
+      value.length < difficulty.sequence_length
+    end
+
+    def long?(value)
+      value.length > difficulty.sequence_length
     end
   end
 end
--- test/sequence_test.rb
+++ test/sequence_test.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require "./test/test_helper"
-require "mastermind_generator/difficulty"
-require "mastermind_generator/item"
-require "mastermind_generator/sequence"
 
 class SequenceTest < Minitest::Test
   include MastermindGenerator
@@ -20,20 +17,34 @@ class SequenceTest < Minitest::Test
   end
 
   def test_it_exists
-    sequence = Sequence.new(Difficulty.new(:beginner))
+    sequence = Sequence.new(Difficulty.new(:beginner), "rrgg")
 
     assert_instance_of Sequence, sequence
   end
 
+  def test_it_has_value
+    sequence = Sequence.new(Difficulty.new(:advanced), "rrggbbpo")
+
+    assert_equal "rrggbbpo", sequence.value
+  end
+
+  def test_it_raises_error_when_its_invalid
+    diff = Difficulty.new(:advanced)
+
+    assert_raises(SequenceTooShortError) { Sequence.new(diff, "rrgbpo") }
+    assert_raises(SequenceTooLongError) { Sequence.new(diff, "rrgbpoppp") }
+    assert_raises(SequenceHasInvalidCharacterError) { Sequence.new(diff, "rrgbpxpp") }
+  end
+
   def test_it_has_items
-    sequence = Sequence.new(Difficulty.new(:advanced))
+    sequence = Sequence.new(Difficulty.new(:advanced), "rgbyporg")
 
     assert_respond_to sequence, :items
     assert sequence.items.all? { _1.instance_of? Item }
   end
 
   def test_it_has_codes
-    sequence = Sequence.new(Difficulty.new(:intermediate))
+    sequence = Sequence.new(Difficulty.new(:intermediate), "rgbyrg")
 
     assert_respond_to sequence, :codes
     assert_equal %w[r g b y p], sequence.codes
