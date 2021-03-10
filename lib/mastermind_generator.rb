4296cb8fb12f6c50a1be2d55e91536c70d2491b4
Sıtkı Bağdat <sbagdat@gmail.com>
2021-03-10 04:08:21 +0300

    feat: Make gem configurable
    - Add options to configure
      - colors
      - difficulties

--- lib/mastermind_generator.rb
+++ lib/mastermind_generator.rb
@@ -2,7 +2,29 @@
 
 require_relative "mastermind_generator/version"
 
+# Mastermind game generator
 module MastermindGenerator
-  class Error < StandardError; end
-  # Your code goes here...
+  # Configuration for the gem
+  class Configuration
+    attr_accessor :colors, :difficulties
+
+    def initialize
+      @colors = nil
+      @difficulties = nil
+    end
+  end
+
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def reset
+      @configuration = Configuration.new
+    end
+
+    def configure
+      yield configuration
+    end
+  end
 end
